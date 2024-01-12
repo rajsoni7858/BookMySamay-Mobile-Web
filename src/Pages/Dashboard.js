5c1768c83ee85d547b1c31e7f9fa540e72e0ba6f
rajsoni7858 <rajsoni7858@gmail.com>
2024-01-12 21:28:35 +0530

    Shop Page work

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,16 +1,34 @@
-import React from "react";
-import { Typography, Row, Col, Card, Statistic } from "antd";
+import React, { useState, useEffect } from "react";
+import { Typography, Row, Col, Card, Statistic, Flex } from "antd";
 
 const { Title } = Typography;
 
 const Dashboard = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString();
+  const formattedTime = currentTime.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const userName = "Raj Soni "; // Replace with the actual user's name
+
   return (
     <Row
       gutter={[48, 16]}
       justify="center"
       align="middle"
-      style={{ marginTop: "20px" }}
+      style={{ marginTop: "20px", padding: 20 }}
     >
+      <Col span={24} style={{ textAlign: "left" }}>
+        <Title level={3} style={{ color: "black" }}>
+          Hello {userName}
+        </Title>
+        <Title level={5} style={{ color: "black", marginRight: "16px" }}>
+          {formattedTime}
+          {formattedDate}
+        </Title>
+      </Col>
       <Col xs={12} sm={12} md={12} lg={8} xl={6}>
         <Card bordered={false}>
           <div>
@@ -18,6 +36,8 @@ const Dashboard = () => {
               src={require("../../src/Assets/Images/Group.png")}
               alt="Group Member Icon"
               style={{
+                display: "Flex",
+                justifyContent: "left",
                 width: 25,
                 height: 25,
               }}
@@ -39,12 +59,21 @@ const Dashboard = () => {
       </Col>
 
       <Col xs={12} sm={12} md={12} lg={8} xl={6}>
-        <Card bordered={false}>
+        <Card
+          bordered={false}
+          style={{
+            borderradius: "14px",
+            boxshadow: "5px 5px 10px 0 rgba(0, 0, 0, 0.5)",
+          }}
+        >
           <div>
             <img
               src={require("../../src/Assets/Images/Check.png")}
               alt="Group Member Icon"
               style={{
+                display: "Flex",
+                justifyContent: "left",
+                paddingleft: 20,
                 width: 25,
                 height: 25,
               }}
@@ -72,6 +101,8 @@ const Dashboard = () => {
               src={require("../../src/Assets/Images/Cancel.png")}
               alt="Group Member Icon"
               style={{
+                display: "Flex",
+                justifyContent: "left",
                 width: 25,
                 height: 25,
               }}
--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,7 +1,319 @@
-import React from "react";
+import React, { useState } from "react";
+import "../App.css";
+import {
+  Form,
+  Input,
+  Layout,
+  InputNumber,
+  Popconfirm,
+  Table,
+  theme,
+  Button,
+  Typography,
+  Tabs,
+  Grid,
+  Row,
+  Col,
+  TimePicker,
+  Select,
+  Modal,
+} from "antd";
+import {
+  FilterOutlined,
+  SearchOutlined,
+  EditOutlined,
+  SaveOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 
-function Shop() {
-  return <div>Shop</div>;
+const { useBreakpoint } = Grid;
+const { Option } = Select;
+const { TabPane } = Tabs;
+
+const originData = [];
+for (let i = 0; i < 100; i++) {
+  originData.push({
+    key: i.toString(),
+    ShopName: `raj ${i}`,
+    MobileNo: "9427778440",
+    location: `London Park no. ${i}`,
+  });
 }
 
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode =
+    inputType === "number" ? (
+      <InputNumber />
+    ) : inputType === "time" ? (
+      <TimePicker format="HH:mm" />
+    ) : (
+      <Input />
+    );
+
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
+
+const Shop = () => {
+  const { Header, Sider, Content } = Layout;
+  const [collapsed, setCollapsed] = useState(false);
+  const [form] = Form.useForm();
+  const [data, setData] = useState(originData);
+  const [editingKey, setEditingKey] = useState("");
+  const isEditing = (record) => record.key === editingKey;
+
+  const edit = (record) => {
+    form.setFieldsValue({
+      ShopName: "",
+      MobileNo: "",
+      address: "",
+      setLocation: "",
+      workingDays: "",
+      holidayInWeek: "",
+      openTime: "",
+      closeTime: "",
+      ...record,
+    });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey("");
+  };
+
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setData(newData);
+        setEditingKey("");
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey("");
+      }
+    } catch (errInfo) {
+      console.log("Validate Failed:", errInfo);
+    }
+  };
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const [additionalFieldsModalVisible, setAdditionalFieldsModalVisible] =
+    useState(false);
+  const [selectedRecord, setSelectedRecord] = useState(null);
+
+  const showAdditionalFieldsModal = (record) => {
+    setAdditionalFieldsModalVisible(true);
+    setSelectedRecord(record);
+  };
+
+  const handleAdditionalFieldsOk = () => {
+    // Handle saving additional fields data
+    setAdditionalFieldsModalVisible(false);
+  };
+
+  const handleAdditionalFieldsCancel = () => {
+    setAdditionalFieldsModalVisible(false);
+  };
+
+  const columns = [
+    {
+      title: "ShopName",
+      dataIndex: "ShopName",
+      width: "25%",
+      editable: true,
+    },
+    {
+      title: "MobileNo",
+      dataIndex: "MobileNo",
+      width: "25%",
+      editable: true,
+    },
+    {
+      title: "Location",
+      dataIndex: "setLocation",
+      width: "25%",
+      editable: true,
+    },
+
+    {
+      title: "operation",
+      dataIndex: "operation",
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link
+              onClick={() => save(record.key)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              <SaveOutlined />
+            </Typography.Link>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>
+                <CloseOutlined />
+              </a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <span>
+            <Typography.Link
+              disabled={editingKey !== ""}
+              onClick={() => showAdditionalFieldsModal(record)}
+              style={{ marginRight: 8 }}
+            >
+              <EditOutlined />
+            </Typography.Link>
+          </span>
+        );
+      },
+    },
+  ];
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.inputType || "text",
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+
+  const history = useHistory();
+
+  const screens = useBreakpoint();
+
+  return (
+    <div style={{ padding: 0 }}>
+      <Content
+        style={{
+          margin: "20px",
+          padding: 0,
+          minHeight: 280,
+          background: "transparent",
+          borderRadius: borderRadiusLG,
+        }}
+      >
+        <Layout style={{ background: "transparent", borderRadius: 15 }}>
+          <Content>
+            <Row gutter={[16, 16]}>
+              <Col xs={24} sm={12} md={24} lg={24} xl={24}>
+                <div style={{ width: "100%", padding: 0 }}>
+                  <Form form={form} component={false}>
+                    <Table
+                      components={{
+                        body: {
+                          cell: EditableCell,
+                        },
+                      }}
+                      bordered
+                      dataSource={data}
+                      columns={mergedColumns}
+                      rowClassName="editable-row"
+                      pagination={{
+                        pageSize: 6, // Add this line to specify the number of rows per page
+                        onChange: cancel,
+                      }}
+                    />
+                  </Form>
+                </div>
+              </Col>
+              {/* Add more Col components for responsiveness */}
+            </Row>
+          </Content>
+          <div style={{ padding: 0 }}>
+            {/* ... (previous code) */}
+
+            {/* Additional Fields Modal */}
+            <Modal
+              title="Here you need to fill the shop details"
+              visible={additionalFieldsModalVisible}
+              onOk={handleAdditionalFieldsOk}
+              onCancel={handleAdditionalFieldsCancel}
+            >
+              {/* Form fields for additional information */}
+              <Form>
+                {/* Use form fields for shop owner, location, set location, working days, holiday in week, open time, close time */}
+                {/* Example: */}
+                <Form.Item label="Shop Name" name="shopName">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Shop Owner" name="shopOwner">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Shop Location" name="shopLocation">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Set Location" name="setLocation">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Working Days" name="workingdays">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Holiday in week" name="holidayinweek">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Opening Time" name="openingtime">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="Closing Time" name="closingtime">
+                  <Input />
+                </Form.Item>
+                {/* ... (add other form fields) */}
+              </Form>
+            </Modal>
+          </div>
+        </Layout>
+      </Content>
+    </div>
+  );
+};
 export default Shop;
