9afb8e16eaf5b479eedeb6959478af6a27692f94
Ronak98 <sonironak0906@gmail.com>
2024-02-25 22:33:19 +0530

    Add package changes

--- src/Pages/Shops/ServicesComponent.jsx
+++ src/Pages/Shops/ServicesComponent.jsx
@@ -5,6 +5,7 @@ import "./Services.css";
 
 const { TabPane } = Tabs;
 const { Panel } = Collapse;
+const { TextArea } = Input;
 
 const sampleData = [
   {
@@ -117,135 +118,280 @@ const ServicesComponent = () => {
         >
           {data.map((tab, tabIndex) => (
             <TabPane tab={tab.tab} key={tabIndex}>
-              {tab.services.map((service, serviceIndex) => (
-                <Collapse
-                  key={serviceIndex}
-                  bordered={false}
-                  className="services"
-                  style={{ marginBottom: "1rem" }}
-                  defaultActiveKey={"0"}
-                >
-                  <Panel
-                    header={service.type}
+              {tab.services.map((service, serviceIndex) =>
+                tab.tab === "Package" ? (
+                  <Collapse
                     key={serviceIndex}
-                    showArrow={false}
+                    bordered={false}
                     className="services"
+                    defaultActiveKey={"0"}
                   >
-                    {service.sub_services.map((subService, subServiceIndex) => (
+                    <Panel
+                      header={service.type}
+                      key={serviceIndex}
+                      showArrow={false}
+                      className="services"
+                    >
+                      {/* Loop through sub services */}
+                      {service.sub_services.map(
+                        (subService, subServiceIndex) => (
+                          <div
+                            key={subServiceIndex}
+                            style={{
+                              paddingBottom: "0.4rem",
+                              marginBottom: "0.8rem",
+                              borderBottom: "1px solid #858181",
+                            }}
+                          >
+                            {/* Input for package name */}
+                            <Input
+                              placeholder="Package Name"
+                              value={subService.packageName}
+                              onChange={(e) =>
+                                handleInputChange(
+                                  e.target.value,
+                                  tabIndex,
+                                  serviceIndex,
+                                  subServiceIndex,
+                                  "packageName"
+                                )
+                              }
+                              style={{
+                                marginBottom: "0.5rem",
+                                fontFamily: "Poppins",
+                                borderRadius: 8,
+                                padding: "0.4rem",
+                                paddingLeft: "0.6rem",
+                                border: "1px solid #1C4792",
+                              }}
+                            />
+                            {/* Inputs for time and charge */}
+                            <div
+                              style={{
+                                display: "flex",
+                                marginBottom: "0.5rem",
+                              }}
+                            >
+                              <InputNumber
+                                placeholder="Time in min"
+                                value={subService.time}
+                                onChange={(value) =>
+                                  handleInputChange(
+                                    value,
+                                    tabIndex,
+                                    serviceIndex,
+                                    subServiceIndex,
+                                    "time"
+                                  )
+                                }
+                                style={{
+                                  width: "50%",
+                                  marginRight: "0.5rem",
+                                  fontFamily: "Poppins",
+                                  borderRadius: 8,
+                                  padding: "0.1rem",
+                                  border: "1px solid #1C4792",
+                                }}
+                              />
+                              <InputNumber
+                                placeholder="Charges"
+                                value={subService.charge}
+                                onChange={(value) =>
+                                  handleInputChange(
+                                    value,
+                                    tabIndex,
+                                    serviceIndex,
+                                    subServiceIndex,
+                                    "charge"
+                                  )
+                                }
+                                style={{
+                                  width: "50%",
+                                  fontFamily: "Poppins",
+                                  borderRadius: 8,
+                                  padding: "0.1rem",
+                                  border: "1px solid #1C4792",
+                                }}
+                              />
+                            </div>
+                            {/* Textarea for package description */}
+                            <TextArea
+                              placeholder="Enter list of all services to add in a single package. 
+                              Ex:- Hair, Beard, Facial, Massage."
+                              autoSize={{ minRows: 3, maxRows: 5 }}
+                              style={{
+                                marginBottom: "0.5rem",
+                                fontFamily: "Poppins",
+                                borderRadius: 8,
+                                padding: "0.35rem",
+                                paddingLeft: "0.55rem",
+                                border: "1px solid #1C4792",
+                              }}
+                            />
+                          </div>
+                        )
+                      )}
+
+                      {/* Add Other Services button */}
                       <div
-                        key={subServiceIndex}
                         style={{
                           display: "flex",
                           alignItems: "center",
-                          justifyContent: "space-between",
-                          marginBottom: "0.5rem",
+                          justifyContent: "center",
                         }}
                       >
-                        {!subService.name ? (
-                          <Input
-                            placeholder="Enter other services"
-                            value={subService.name}
-                            onChange={(e) =>
-                              handleInputChange(
-                                e.target.value,
-                                tabIndex,
-                                serviceIndex,
-                                subServiceIndex,
-                                "name"
-                              )
-                            }
-                            style={{
-                              width: "42%",
-                              fontFamily: "Poppins",
-                              borderRadius: 8,
-                              padding: "0.35rem",
-                              border: "1px solid #1C4792",
-                            }}
-                          />
-                        ) : (
-                          <div style={{ width: "42%", fontFamily: "Poppins" }}>
-                            {subService.name}
-                          </div>
-                        )}
-                        <InputNumber
-                          placeholder="Time in min"
-                          value={subService.time}
-                          onChange={(value) =>
-                            handleInputChange(
-                              value,
-                              tabIndex,
-                              serviceIndex,
-                              subServiceIndex,
-                              "time"
-                            )
-                          }
+                        <Button
+                          type="text"
+                          onClick={() => handleAddExtra(tabIndex, serviceIndex)}
                           style={{
-                            width: "22%",
-                            borderRadius: 8,
-                            padding: "0.1rem",
-                            border: "1px solid #1C4792",
+                            borderRadius: "8px",
+                            backgroundColor: "rgb(235 235 235)",
+                            fontFamily: "Poppins",
+                            paddingTop: "5px",
                           }}
-                        />
-                        <InputNumber
-                          placeholder="Charges"
-                          value={subService.charge}
-                          onChange={(value) =>
-                            handleInputChange(
-                              value,
-                              tabIndex,
-                              serviceIndex,
-                              subServiceIndex,
-                              "charge"
-                            )
-                          }
-                          style={{
-                            width: "22%",
-                            borderRadius: 8,
-                            padding: "0.1rem",
-                            border: "1px solid #1C4792",
-                          }}
-                        />
-                        <Checkbox
-                          checked={subService.selected}
-                          onChange={(e) =>
-                            handleCheckboxChange(
-                              tabIndex,
-                              serviceIndex,
-                              subServiceIndex,
-                              e.target.checked
-                            )
-                          }
-                        />
+                        >
+                          <PlusCircleOutlined
+                            size="large"
+                            style={{ fontSize: "16px" }}
+                          />{" "}
+                          Add Package
+                        </Button>
                       </div>
-                    ))}
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        marginTop: "1rem",
-                      }}
+                    </Panel>
+                  </Collapse>
+                ) : (
+                  <Collapse
+                    key={serviceIndex}
+                    bordered={false}
+                    className="services"
+                    defaultActiveKey={"0"}
+                  >
+                    <Panel
+                      header={service.type}
+                      key={serviceIndex}
+                      showArrow={false}
+                      className="services"
                     >
-                      <Button
-                        type="text"
-                        onClick={() => handleAddExtra(tabIndex, serviceIndex)}
+                      {service.sub_services.map(
+                        (subService, subServiceIndex) => (
+                          <div
+                            key={subServiceIndex}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "space-between",
+                              marginBottom: "0.5rem",
+                            }}
+                          >
+                            {!subService.name ? (
+                              <Input
+                                placeholder="Enter other services"
+                                value={subService.name}
+                                onChange={(e) =>
+                                  handleInputChange(
+                                    e.target.value,
+                                    tabIndex,
+                                    serviceIndex,
+                                    subServiceIndex,
+                                    "name"
+                                  )
+                                }
+                                style={{
+                                  width: "42%",
+                                  fontFamily: "Poppins",
+                                  borderRadius: 8,
+                                  padding: "0.35rem",
+                                  border: "1px solid #1C4792",
+                                }}
+                              />
+                            ) : (
+                              <div
+                                style={{ width: "42%", fontFamily: "Poppins" }}
+                              >
+                                {subService.name}
+                              </div>
+                            )}
+                            <InputNumber
+                              placeholder="Time in min"
+                              value={subService.time}
+                              onChange={(value) =>
+                                handleInputChange(
+                                  value,
+                                  tabIndex,
+                                  serviceIndex,
+                                  subServiceIndex,
+                                  "time"
+                                )
+                              }
+                              style={{
+                                width: "22%",
+                                borderRadius: 8,
+                                padding: "0.1rem",
+                                border: "1px solid #1C4792",
+                              }}
+                            />
+                            <InputNumber
+                              placeholder="Charges"
+                              value={subService.charge}
+                              onChange={(value) =>
+                                handleInputChange(
+                                  value,
+                                  tabIndex,
+                                  serviceIndex,
+                                  subServiceIndex,
+                                  "charge"
+                                )
+                              }
+                              style={{
+                                width: "22%",
+                                borderRadius: 8,
+                                padding: "0.1rem",
+                                border: "1px solid #1C4792",
+                              }}
+                            />
+                            <Checkbox
+                              checked={subService.selected}
+                              onChange={(e) =>
+                                handleCheckboxChange(
+                                  tabIndex,
+                                  serviceIndex,
+                                  subServiceIndex,
+                                  e.target.checked
+                                )
+                              }
+                            />
+                          </div>
+                        )
+                      )}
+                      <div
                         style={{
-                          borderRadius: "8px",
-                          backgroundColor: "rgb(235 235 235)",
-                          fontFamily: "Poppins",
-                          paddingTop: "5px",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          marginTop: "1rem",
                         }}
                       >
-                        <PlusCircleOutlined
-                          size="large"
-                          style={{ fontSize: "16px" }}
-                        />{" "}
-                        Add Other Services
-                      </Button>
-                    </div>
-                  </Panel>
-                </Collapse>
-              ))}
+                        <Button
+                          type="text"
+                          onClick={() => handleAddExtra(tabIndex, serviceIndex)}
+                          style={{
+                            borderRadius: "8px",
+                            backgroundColor: "rgb(235 235 235)",
+                            fontFamily: "Poppins",
+                            paddingTop: "5px",
+                          }}
+                        >
+                          <PlusCircleOutlined
+                            size="large"
+                            style={{ fontSize: "16px" }}
+                          />{" "}
+                          Add Other Services
+                        </Button>
+                      </div>
+                    </Panel>
+                  </Collapse>
+                )
+              )}
             </TabPane>
           ))}
         </Tabs>
